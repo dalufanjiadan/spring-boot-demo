9f7d912cd344e519bba58b858f36db562800edb3
dalufanjiadan <dalufanjiadan@qq.com>
2020-06-26 14:28:47 +0800

    echart

--- vue-demo/src/main.js
+++ vue-demo/src/main.js
@@ -4,9 +4,11 @@ import router from "./router";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import { get, post } from "./util/httpUtil";
+import echarts from "echarts";
 
 Vue.use(ElementUI);
 Vue.prototype.$http = { get, post };
+Vue.prototype.$echarts = echarts;
 
 Vue.config.productionTip = false;
 
--- vue-demo/src/view/Home.vue
+++ vue-demo/src/view/Home.vue
@@ -2,6 +2,7 @@
 	<div>
 		<h1>Home</h1>
 		<el-button type="primary" v-on:click="hello">test button</el-button>
+		<div id="char1" style="width: 600px;height:400px;"></div>
 	</div>
 </template>
 
@@ -20,10 +21,115 @@ export default {
 		},
 		hello() {
 			console.log("hello world11");
+			let data = [1, 2, 3, 4, 5];
 			this.$http.get("/api/v1/test/hello").then((res) => {
 				console.log(res);
 				console.log(res.data);
 			});
+
+			let myChart = this.$echarts.init(document.getElementById("char1"));
+
+			// 指定图表的配置项和数据
+			var option = {
+				title: {
+					text: "ECharts 入门示例",
+				},
+				tooltip: {},
+				legend: {
+					data: ["销量"],
+				},
+				xAxis: {
+					data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"],
+				},
+				yAxis: {},
+				series: [
+					{
+						name: "销量",
+						type: "bar",
+						data: [5, 20, 36, 10, 10, 20],
+					},
+				],
+			};
+
+			// 使用刚指定的配置项和数据显示图表。
+			myChart.setOption(option);
+
+			option = {
+				xAxis: {
+					type: "category",
+					data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+				},
+				yAxis: {
+					type: "value",
+				},
+				series: [
+					{
+						data: [820, 932, 901, 934, 1290, 1330, 1320],
+						type: "line",
+					},
+				],
+			};
+			myChart.setOption(option);
+
+			option = {
+				xAxis: {
+					type: "category",
+					data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+				},
+				yAxis: {
+					type: "value",
+				},
+				series: [
+					{
+						data: data,
+						type: "line",
+					},
+				],
+			};
+			myChart.setOption(option);
+
+			option = {
+				tooltip: {
+					trigger: "item",
+					formatter: "{a} <br/>{b}: {c} ({d}%)",
+				},
+				legend: {
+					orient: "vertical",
+					left: 10,
+					data: ["直接访问", "邮件营销", "联盟广告", "视频广告", "搜索引擎"],
+				},
+				series: [
+					{
+						name: "访问来源",
+						type: "pie",
+						radius: ["50%", "70%"],
+						avoidLabelOverlap: false,
+						label: {
+							show: false,
+							position: "center",
+						},
+						emphasis: {
+							label: {
+								show: true,
+								fontSize: "30",
+								fontWeight: "bold",
+							},
+						},
+						labelLine: {
+							show: false,
+						},
+						data: [
+							{ value: 335, name: "直接访问" },
+							{ value: 310, name: "邮件营销" },
+							{ value: 234, name: "联盟广告" },
+							{ value: 135, name: "视频广告" },
+							{ value: 1548, name: "搜索引擎" },
+						],
+					},
+				],
+			};
+
+			myChart.setOption(option);
 		},
 	},
 	props: {
