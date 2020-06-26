c0fde0888819a4a714188d2f1f2f729e5008cfb3
dalufanjiadan <dalufanjiadan@qq.com>
2020-06-26 18:11:25 +0800

    menu

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,29 +1,35 @@
 <template>
-	<div>
-		<el-menu
-			:default-active="activeIndex"
-			class="el-menu-demo"
-			mode="horizontal"
-			@select="handleSelect"
-		>
-			<el-menu-item index="1"> <router-link to="/home">Home</router-link></el-menu-item>
-			<el-submenu index="2">
-				<template slot="title">我的工作台</template>
-				<el-menu-item index="2-1">选项1</el-menu-item>
-				<el-menu-item index="2-2">选项2</el-menu-item>
-				<el-menu-item index="2-3">选项3</el-menu-item>
+	<el-container>
+		<el-header>
+			<el-menu
+				:default-active="activeIndex"
+				class="el-menu-demo"
+				mode="horizontal"
+				@select="handleSelect"
+				router="true"
+			>
+				<el-menu-item index="1" route="/Home"> Home</el-menu-item>
+				<el-submenu index="2">
+					<template slot="title">我的工作台</template>
+					<el-menu-item index="2-1">选项1</el-menu-item>
+					<el-menu-item index="2-2">选项2</el-menu-item>
+					<el-menu-item index="2-3">选项3</el-menu-item>
+				</el-submenu>
 				<el-submenu index="2-4">
 					<template slot="title">选项4</template>
 					<el-menu-item index="2-4-1">选项1</el-menu-item>
 					<el-menu-item index="2-4-2">选项2</el-menu-item>
 					<el-menu-item index="2-4-3">选项3</el-menu-item>
 				</el-submenu>
-			</el-submenu>
-			<el-menu-item index="3" disabled>消息中心</el-menu-item>
-			<el-menu-item index="4"> <router-link to="/about">About</router-link></el-menu-item>
-		</el-menu>
-		<router-view />
-	</div>
+				<el-menu-item index="3" disabled>消息中心</el-menu-item>
+				<el-menu-item index="4" route="/about"> About</el-menu-item>
+			</el-menu>
+		</el-header>
+		<el-main>
+			<router-view />
+		</el-main>
+		<el-footer>Footer</el-footer>
+	</el-container>
 </template>
 
 <script>
