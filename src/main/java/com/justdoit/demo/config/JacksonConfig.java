12f7d18ceb1cb3833e9a8fb91e45a36dfa2349ba
dalufanjiadan <dalufanjiadan@qq.com>
2020-07-06 18:29:13 +0800

    jackson config

---
+++ src/main/java/com/justdoit/demo/config/JacksonConfig.java
@@ -0,0 +1,26 @@
+package com.justdoit.demo.config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+@Configuration
+public class JacksonConfig {
+
+	private static final String DATE_FORMAT = "yyyy-MM-dd";
+	private static final String DATETIME_FORMAT = "yyyy-MM-dd HH:mm";
+
+	@Bean
+	public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+		return builder -> {
+			builder.simpleDateFormat(DATETIME_FORMAT);
+			builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(DATE_FORMAT)));
+			builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DATETIME_FORMAT)));
+		};
+	}
+}
